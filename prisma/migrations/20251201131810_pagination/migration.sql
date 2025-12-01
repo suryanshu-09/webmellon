2552f27d1f22e3910a76dac2294f4ce7ef4ddffa
suryanshu-09 <suryanshusaini@gmail.com>
2025-12-01 20:37:52 +0530

    fix db migration

--- prisma/migrations/20251201131810_pagination/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Catalogue" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "preferences" JSONB DEFAULT '{"paginationType": "pages", "itemsPerPage": 10}';
