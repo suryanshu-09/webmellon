af94e9a2f43e0755d9ff89d94d1306ebbcc68b28
suryanshu-09 <suryanshusaini@gmail.com>
2025-12-15 20:17:03 +0530

    pls work

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import path from 'node:path'
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
+  seed: {
+    command: 'tsx prisma/seed.ts',
+  },
+})
